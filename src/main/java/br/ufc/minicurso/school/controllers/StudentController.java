ac8f4e7817bb409217819662a21ef1ee9f94b771
tiago <tiago@gmail.com>
2019-04-04 18:43:26 -0600

    Alterando as coisas para teste

--- src/main/java/br/ufc/minicurso/school/controllers/StudentController.java
+++ src/main/java/br/ufc/minicurso/school/controllers/StudentController.java
@@ -1,30 +1,16 @@
 package br.ufc.minicurso.school.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.ufc.minicurso.school.model.Student;
-import br.ufc.minicurso.school.service.StudentService;
-
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
-	
-	@Autowired
-	StudentService studentService;
 
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public Iterable<Student> getStudents(){
-		return studentService.getStudents();
+	public String getStudents() {
+		return "Student Api";
 	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public Student addStudent(@RequestBody Student student) {
-		studentService.addStudent(student);
-		return student;
-	}
-
 }
